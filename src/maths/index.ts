6af541f87b78e95e2a35406b1d9f23fb194efa26
willclarktech <willclarktech@users.noreply.github.com>
2019-11-18 16:21:24 +0000

    :seedling: maths: Add subtract function

--- src/maths/index.ts
+++ src/maths/index.ts
@@ -1,5 +1,5 @@
 export { CostFunction, costFunctionMap, CostFunctionName } from "./cost-functions";
-export { add, Matrix, mean, multiply, sum, transpose, Vector } from "./linear";
+export { add, Matrix, mean, multiply, subtract, sum, transpose, Vector } from "./linear";
 export { NonLinearFunction, nonLinearFunctionMap, NonLinearFunctionName } from "./non-linear-functions";
 export {
 	NormalisationFunction,
--- src/maths/linear.ts
+++ src/maths/linear.ts
@@ -3,6 +3,8 @@ export type Matrix = readonly Vector[];
 
 export const add = (a: number, b: number): number => a + b;
 
+export const subtract = (a: number, b: number): number => a - b;
+
 export const multiply = (a: number, b: number): number => a * b;
 
 export const sum = (ns: Vector): number => ns.reduce((total, n) => total + n, 0);
