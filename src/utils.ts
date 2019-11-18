c35a78f76d5d70d90949d71ba5029cd22154b25a
willclarktech <willclarktech@users.noreply.github.com>
2019-11-18 17:02:26 +0000

    :seedling: utils: Add unzip function

--- src/utils.ts
+++ src/utils.ts
@@ -7,5 +7,14 @@ export const getRandomNumber = (min = -1, max = 1): number => {
 	return seed * range + min;
 };
 
+export const unzip = <T, U>(zipped: readonly (readonly [T, U])[]): readonly [readonly T[], readonly U[]] =>
+	zipped.reduce(
+		([as, bs]: readonly [readonly T[], readonly U[]], [a, b]: readonly [T, U]) => [
+			[...as, a],
+			[...bs, b],
+		],
+		[[], []],
+	);
+
 export const zipWith = <T, U, V>(fn: (a: T, b: U) => V, as: readonly T[], bs: readonly U[]): readonly V[] =>
 	as.map((a, i) => fn(a, bs[i]));
