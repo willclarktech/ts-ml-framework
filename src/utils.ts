6f50246bef50caf1a02fc2cffc26076afd5e65f8
willclarktech <willclarktech@users.noreply.github.com>
2019-11-18 17:52:33 +0000

    :seedling: utils: Add zip function

--- src/utils.ts
+++ src/utils.ts
@@ -16,5 +16,8 @@ export const unzip = <T, U>(zipped: readonly (readonly [T, U])[]): readonly [rea
 		[[], []],
 	);
 
+export const zip = <T, U>(as: readonly T[], bs: readonly U[]): readonly (readonly [T, U])[] =>
+	as.map((a, i) => [a, bs[i]]);
+
 export const zipWith = <T, U, V>(fn: (a: T, b: U) => V, as: readonly T[], bs: readonly U[]): readonly V[] =>
 	as.map((a, i) => fn(a, bs[i]));
