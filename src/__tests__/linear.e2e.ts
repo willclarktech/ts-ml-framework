6eb2b1654ea118d3087af01531d66ae5d7dda8b4
willclarktech <willclarktech@users.noreply.github.com>
2019-11-22 10:12:42 +0000

    :white_check_mark: e2e: Add multi input, multi output linear e2e test

--- src/__tests__/linear.e2e.ts
+++ src/__tests__/linear.e2e.ts
@@ -5,7 +5,7 @@ import { flatten, nest } from "../utils";
 
 const logFrequency = 0;
 
-test("single input, single output: y = 5x + 4", () => {
+test("single input, single output: f(x) = 5x + 4", () => {
 	const trainInputs = nest([-5, -3, -1, 1, 3, 5]);
 	const trainOutputs = nest([-21, -11, -1, 9, 19, 29]);
 	const testInputs = nest([-4, -2, 0, 2, 4]);
@@ -33,7 +33,7 @@ test("single input, single output: y = 5x + 4", () => {
 	expect(error).toBeLessThan(0.01);
 });
 
-test("single input, multi output: y = [5x + 4, 0.2x - 7]", () => {
+test("single input, multi output: f(x) = [5x + 4, 0.2x - 7]", () => {
 	const trainInputs = nest([-5, -3, -1, 1, 3, 5]);
 	const trainOutputs = [
 		[-21, -8],
@@ -74,13 +74,13 @@ test("single input, multi output: y = [5x + 4, 0.2x - 7]", () => {
 	expect(error).toBeLessThan(0.01);
 });
 
-test("multi input, single output: z = 5x - 0.2y + 4", () => {
+test("multi input, single output: f(x, y) = 5x - 0.2y + 4", () => {
 	const trainNs = [-5, -3, -1, 1, 3, 5];
 	const trainInputs = flatten(trainNs.map(x => trainNs.map(y => [x, y])));
-	const trainOutputs = nest(trainInputs.map(([x, y]) => 5 * x - 0.2 * y + 4));
+	const trainOutputs = trainInputs.map(([x, y]) => [5 * x - 0.2 * y + 4]);
 	const testNs = [-4, -2, 0, 2, 4];
 	const testInputs = flatten(testNs.map(x => testNs.map(y => [x, y])));
-	const testOutputs = nest(testInputs.map(([x, y]) => 5 * x - 0.2 * y + 4));
+	const testOutputs = testInputs.map(([x, y]) => [5 * x - 0.2 * y + 4]);
 	const specifications = [
 		{
 			kind: LayerKind.Input as const,
@@ -89,6 +89,36 @@ test("multi input, single output: z = 5x - 0.2y + 4", () => {
 		{
 			kind: LayerKind.Linear as const,
 			width: 1,
+		},
+		{
+			kind: LayerKind.Cost as const,
+			fn: "mean-squared-error" as const,
+		},
+	];
+	const initialNetwork = createNetwork(specifications);
+	const iterations = 100;
+	const alpha = 0.001;
+	const trained = train(initialNetwork, trainOutputs, trainInputs, iterations, alpha, logFrequency);
+	const tested = activateNetwork(testOutputs, testInputs, trained);
+	const error = getAverageError(tested);
+	expect(error).toBeLessThan(0.01);
+});
+
+test("multi input, multi output: f(x, y) = [5x - 0.2y + 4, -0.2x + 3y - 8]", () => {
+	const trainNs = [-5, -3, -1, 1, 3, 5];
+	const trainInputs = flatten(trainNs.map(x => trainNs.map(y => [x, y])));
+	const trainOutputs = trainInputs.map(([x, y]) => [5 * x - 0.2 * y + 4, -0.2 * x + 3 * y - 8]);
+	const testNs = [-4, -2, 0, 2, 4];
+	const testInputs = flatten(testNs.map(x => testNs.map(y => [x, y])));
+	const testOutputs = testInputs.map(([x, y]) => [5 * x - 0.2 * y + 4, -0.2 * x + 3 * y - 8]);
+	const specifications = [
+		{
+			kind: LayerKind.Input as const,
+			width: 2,
+		},
+		{
+			kind: LayerKind.Linear as const,
+			width: 2,
 		},
 		{
 			kind: LayerKind.Cost as const,
