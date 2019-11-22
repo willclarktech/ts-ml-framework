f5de6a2c37e1e8ba00ef9c79b3b384780a3ec342
willclarktech <willclarktech@users.noreply.github.com>
2019-11-22 10:08:24 +0000

    :seedling: utils: Add flatten function

--- src/utils.ts
+++ src/utils.ts
@@ -7,6 +7,9 @@ export const getRandomNumber = (min = -1, max = 1): number => {
 	return seed * range + min;
 };
 
+export const flatten = <T>(array: readonly (readonly T[])[]): readonly T[] =>
+	array.reduce((flattened, next) => [...flattened, ...next], []);
+
 export const nest = (ns: readonly number[]): readonly (readonly number[])[] => ns.map(n => [n]);
 
 export const unzip = <T, U>(zipped: readonly (readonly [T, U])[]): readonly [readonly T[], readonly U[]] =>
