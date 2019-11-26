3818ee3b857e0556e7b8e1352610e6922c4d0255
willclarktech <willclarktech@users.noreply.github.com>
2019-11-26 12:51:38 +0000

    :seedling: maths: Add cross entropy cost function

--- src/maths/cost-functions.ts
+++ src/maths/cost-functions.ts
@@ -1,10 +1,25 @@
 import { zipWith } from "../utils";
-import { mean, Vector } from "./linear";
+import { mean, sum, Vector } from "./linear";
 
 export interface CostFunction {
 	readonly calculate: (expectedInputs: Vector, inputs: Vector) => number;
 	readonly derivative: (expectedInputs: Vector, inputs: Vector) => Vector;
 }
+
+const crossEntropyCalculate = (expectedInput: number, input: number): number =>
+	-(expectedInput * Math.log(input));
+const crossEntropyDerivative = (expectedInput: number, input: number): number => -(expectedInput / input);
+
+const crossEntropy: CostFunction = {
+	calculate: (expectedInputs: Vector, inputs: Vector) => {
+		if (expectedInputs.length !== inputs.length) {
+			throw new Error("Cannot calculate cost with inputs/expected inputs of different lengths");
+		}
+		return sum(zipWith(crossEntropyCalculate, expectedInputs, inputs));
+	},
+	derivative: (expectedInputs: Vector, inputs: Vector) =>
+		zipWith(crossEntropyDerivative, expectedInputs, inputs),
+};
 
 const squaredErrorCalculate = (expectedInput: number, input: number): number => (input - expectedInput) ** 2;
 const squaredErrorDerivative = (expectedInput: number, input: number): number => 2 * (input - expectedInput);
@@ -20,8 +35,9 @@ const meanSquaredError: CostFunction = {
 		zipWith(squaredErrorDerivative, expectedInputs, inputs),
 };
 
-export type CostFunctionName = "mean-squared-error";
+export type CostFunctionName = "cross-entropy" | "mean-squared-error";
 
 export const costFunctionMap = new Map<CostFunctionName, CostFunction>([
+	["cross-entropy", crossEntropy],
 	["mean-squared-error", meanSquaredError],
 ]);
