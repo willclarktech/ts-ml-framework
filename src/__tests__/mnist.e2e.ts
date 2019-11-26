2913406ea4fb09a3d2e7ac148f1e5fe643996c89
willclarktech <willclarktech@users.noreply.github.com>
2019-11-26 14:05:02 +0000

    :white_check_mark: e2e: Add fully-connected MNIST test

---
+++ src/__tests__/mnist.e2e.ts
@@ -0,0 +1,103 @@
+import { readFileSync } from "fs";
+
+import { LayerKind } from "../layer";
+import { normalisationFunctionMap } from "../maths";
+import { activateNetwork, Batch, createNetwork, OutputInputPair } from "../network";
+import { train } from "../train";
+import { createOneHot, unzip } from "../utils";
+
+const logFrequency = 0;
+
+// const trainDataPath = "./data/mnist/train.csv";
+const trainDataPath = "./data/mnist/train.partial.csv";
+
+const argmax = normalisationFunctionMap.get("argmax");
+if (!argmax) {
+	throw new Error("argmax function not found");
+}
+
+const linearNormalisation = normalisationFunctionMap.get("linear");
+if (!linearNormalisation) {
+	throw new Error("linear normalisation function not found");
+}
+
+const loadMnistData = (path: string): Batch => {
+	const rawData = readFileSync(path, { encoding: "utf8" });
+	// Line 0 is header; last line is blank
+	const lines = rawData.split("\n").slice(1, -1);
+	return lines.map(
+		(line: string): OutputInputPair => {
+			const [nextExpectedOutputIndex, ...nextInput] = line
+				.split(",")
+				.map((n: string): number => parseInt(n, 10));
+			const nextExpectedOutput = createOneHot(nextExpectedOutputIndex);
+			const nextInputNormalised = linearNormalisation.calculate(nextInput);
+			return [nextExpectedOutput, nextInputNormalised];
+		},
+	);
+};
+
+// const printMnistExample = (input: readonly number[]): string => {
+// 	const d = 28;
+// 	return [...new Array(d)]
+// 		.map((_, i) =>
+// 			input
+// 				.slice(i * d, (i + 1) * d)
+// 				.map(n => (n > 127 ? "O" : "."))
+// 				.join(""),
+// 		)
+// 		.join("\n");
+// };
+
+test("mnist", () => {
+	const mnistData = loadMnistData(trainDataPath);
+	const nExamples = 1000;
+	const nTrainData = nExamples * 0.9;
+	const trainData = mnistData.slice(0, nTrainData);
+	const [trainOutputs, trainInputs] = unzip(trainData);
+	// const [testOutputs, testInputs] = [trainOutputs, trainInputs];
+	const testData = mnistData.slice(nTrainData);
+	const [testOutputs, testInputs] = unzip(testData);
+
+	const specifications = [
+		{
+			kind: LayerKind.Input as const,
+			width: 784,
+		},
+		{
+			kind: LayerKind.Linear as const,
+			width: 10,
+		},
+		{
+			kind: LayerKind.Normalisation as const,
+			fn: "softmax" as const,
+		},
+		{
+			kind: LayerKind.Cost as const,
+			fn: "cross-entropy" as const,
+		},
+	];
+	const initialNetwork = createNetwork(specifications);
+	const iterations = 50;
+	const alpha = 0.002;
+	const trained = train(initialNetwork, trainOutputs, trainInputs, iterations, alpha, logFrequency);
+	const trainedFinal = activateNetwork(trainOutputs, trainInputs, trained);
+
+	const trainedGuesses = trainedFinal.layers[trained.layers.length - 2].activationsBatch.map(
+		argmax.calculate,
+	);
+	const trainedCorrect = trainOutputs.reduce((total, trainOutput, i) => {
+		const guess = trainedGuesses[i];
+		return total + Number(trainOutput.every((n, j) => n === guess[j]));
+	}, 0);
+	expect(trainedCorrect / trainOutputs.length).toBeGreaterThan(0.9);
+
+	const tested = activateNetwork(testOutputs, testInputs, trained);
+
+	const guesses = tested.layers[tested.layers.length - 2].activationsBatch.map(argmax.calculate);
+	const correct = testOutputs.reduce((total, testOutput, i) => {
+		const guess = guesses[i];
+		return total + Number(testOutput.every((n, j) => n === guess[j]));
+	}, 0);
+	expect(correct / testOutputs.length).toBeGreaterThan(0.8);
+});
