d7526fb2c744ec7e5c1664a296a8a07ecd135b4c
willclarktech <willclarktech@users.noreply.github.com>
2019-11-26 15:44:23 +0000

    :white_check_mark: e2e: Add hidden layer MNIST test

--- src/__tests__/mnist.e2e.ts
+++ src/__tests__/mnist.e2e.ts
@@ -122,8 +122,69 @@ test("mnist with mini-batches", () => {
 		},
 	];
 	const initialNetwork = createNetwork(specifications);
-	const iterations = 70;
-	const alpha = 0.002;
+	const iterations = 40;
+	const alpha = 0.01;
+	const miniBatchSize = 32;
+	const trained = trainWithMiniBatches(
+		initialNetwork,
+		trainOutputs,
+		trainInputs,
+		iterations,
+		alpha,
+		logFrequency,
+		miniBatchSize,
+	);
+	const trainedFinal = activateNetwork(trainOutputs, trainInputs, trained);
+
+	const trainedGuesses = trainedFinal.layers[trained.layers.length - 2].activationsBatch.map(
+		argmax.calculate,
+	);
+	const trainedCorrect = trainOutputs.reduce((total, trainOutput, i) => {
+		const guess = trainedGuesses[i];
+		return total + Number(trainOutput.every((n, j) => n === guess[j]));
+	}, 0);
+	expect(trainedCorrect / trainOutputs.length).toBeGreaterThan(0.9);
+
+	const tested = activateNetwork(testOutputs, testInputs, trained);
+
+	const guesses = tested.layers[tested.layers.length - 2].activationsBatch.map(argmax.calculate);
+	const correct = testOutputs.reduce((total, testOutput, i) => {
+		const guess = guesses[i];
+		return total + Number(testOutput.every((n, j) => n === guess[j]));
+	}, 0);
+	expect(correct / testOutputs.length).toBeGreaterThan(0.8);
+});
+
+test("mnist with mini-batches and hidden layer", () => {
+	const specifications = [
+		{
+			kind: LayerKind.Input as const,
+			width: 784,
+		},
+		{
+			kind: LayerKind.Linear as const,
+			width: 30,
+		},
+		{
+			kind: LayerKind.NonLinear as const,
+			fn: "tanh" as const,
+		},
+		{
+			kind: LayerKind.Linear as const,
+			width: 10,
+		},
+		{
+			kind: LayerKind.Normalisation as const,
+			fn: "softmax" as const,
+		},
+		{
+			kind: LayerKind.Cost as const,
+			fn: "cross-entropy" as const,
+		},
+	];
+	const initialNetwork = createNetwork(specifications);
+	const iterations = 40;
+	const alpha = 0.01;
 	const miniBatchSize = 32;
 	const trained = trainWithMiniBatches(
 		initialNetwork,
