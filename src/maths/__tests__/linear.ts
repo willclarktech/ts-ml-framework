868dee264132b950698fef072b73b5077b283bbd
willclarktech <willclarktech@users.noreply.github.com>
2019-11-29 13:09:08 +0100

    :seedling: maths: Add shapeOf function

--- src/maths/__tests__/linear.ts
+++ src/maths/__tests__/linear.ts
@@ -1,4 +1,4 @@
-import { add, mean, multiply, subtract, sum, transpose } from "../linear";
+import { add, mean, multiply, shapeOf, subtract, sum, transpose } from "../linear";
 
 const MAX_INACCURACY = 1e-8;
 
@@ -30,6 +30,48 @@ test("mean", () => {
 	const result = mean([2.4, 3.8, -8.9]);
 	const diff = Math.abs(result - -0.9);
 	expect(diff).toBeLessThan(MAX_INACCURACY);
+});
+
+describe("shapeOf", () => {
+	test("works for a number", () => {
+		const n = 9;
+		expect(shapeOf(n)).toStrictEqual([]);
+	});
+
+	test("works for a vector", () => {
+		const v = [1, 2, 3];
+		expect(shapeOf(v)).toStrictEqual([3]);
+	});
+
+	test("works for a matrix", () => {
+		const m = [
+			[1, 2, 3],
+			[4, 5, 6],
+		];
+		expect(shapeOf(m)).toStrictEqual([2, 3]);
+	});
+
+	test("works for a higher-order tensor", () => {
+		const m = [
+			[
+				[1, 2, 3],
+				[4, 5, 6],
+			],
+			[
+				[11, 12, 13],
+				[14, 15, 16],
+			],
+			[
+				[21, 22, 23],
+				[24, 25, 26],
+			],
+			[
+				[31, 32, 33],
+				[34, 35, 36],
+			],
+		];
+		expect(shapeOf(m)).toStrictEqual([4, 2, 3]);
+	});
 });
 
 describe("transpose", () => {
--- src/maths/linear.ts
+++ src/maths/linear.ts
@@ -1,6 +1,7 @@
 export type Vector = readonly number[];
 export type Matrix = readonly Vector[];
-export type Tensor<T extends number | Vector | Matrix> = readonly (readonly T[])[];
+export type SubTensor = number | Vector | Matrix;
+export type Tensor<T extends SubTensor> = readonly (readonly T[])[];
 
 export const add = (a: number, b: number): number => a + b;
 
@@ -11,6 +12,9 @@ export const multiply = (a: number, b: number): number => a * b;
 export const sum = (ns: Vector): number => ns.reduce((total, n) => total + n, 0);
 
 export const mean = (ns: Vector): number => sum(ns) / ns.length;
+
+export const shapeOf = (tensor: Tensor<SubTensor> | SubTensor): readonly number[] =>
+	Array.isArray(tensor) ? [tensor.length, ...shapeOf(tensor[0])] : [];
 
 export const transpose = <T extends number | Vector | Matrix>(matrix: Tensor<T>): Tensor<T> => {
 	if (matrix.length === 0 || matrix[0].length === 0) {
